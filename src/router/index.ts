11cd6fcee97c359a56657829c856b891c81e7ead
sura0111 <sura0111@gmail.com>
2020-12-16 01:18:05 +0900

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import home from '@/views/home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'home',
+    component: home,
   },
 ]
 
